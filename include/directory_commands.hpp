d675db6e110e90781979990e3f02b0a9fa919f84
Rajan Choksi <rachoksi9999@gmail.com>
2025-08-25 14:26:35 +0530

    directory related files and config done

---
+++ include/directory_commands.hpp
@@ -0,0 +1,13 @@
+#ifndef DIRECTORY_COMMANDS_HEADERS
+#define DIRECTORY_COMMANDS_HEADERS
+
+namespace DIRECTORY_COMMANDS
+{
+    class Directory_commands 
+    {
+        public:
+            int create_directory(const char *path);
+    };
+}
+
+#endif
---
+++ src/directory_commands/directory_commands.one.cpp
@@ -0,0 +1,8 @@
+#include "../include/directory_commands.hpp"
+
+using namespace DIRECTORY_COMMANDS;
+
+int Directory_commands::create_directory(const char *path) 
+{
+    
+}
