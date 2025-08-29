0cd224b1483ea9090aad074579527fbadf622625
Rajan Choksi <rachoksi9999@gmail.com>
2025-08-29 22:57:55 +0530

    started working on find pattern command

--- src/utilities/utilities_one.cpp
+++ src/utilities/utilities_one.cpp
@@ -16,12 +16,51 @@ int Utilities_commands::print_text(std::string text)
 
 int Utilities_commands::find_pattern(std::string pattern, const char *path) 
 {
+    // checking file if it exist and also if it is readable
     if (access(path, F_OK) == -1 || access(path, R_OK) == -1) 
     {
         std::cerr << "-> Error occured, " << strerror(errno) << '\n';
         return 1;
     }
 
-    std::cout << "this command prints match patterns" << '\n';
+    // creating file descriptor for opening file to read it later
+    int fd;
+    fd = open(path, O_RDONLY);
+    if (fd == -1) 
+    {
+        std::cerr << "-> Error occured, " << strerror(errno) << '\n';
+        return 1;
+    }
+
+    ssize_t buffer_read;
+    char buffer[128];
+
+    // test code
+    int count = 1;
+
+    while ( (buffer_read = read(fd, buffer, sizeof(buffer))) > 0 ) 
+    {
+        std::cout << count;
+        std::cout << buffer << '\n';
+        count++; 
+    }
+
+    if (buffer_read == -1) 
+    {
+        std::cerr << "-> Error occured, " << strerror(errno) << '\n';
+        if (close(fd) == -1) 
+        {
+            std::cerr << "-> Error occured, " << strerror(errno) << '\n';
+            return 1;   
+        }
+        return 1;
+    }
+
+    if (close(fd) == -1) 
+    {
+        std::cerr << "-> Error occured, " << strerror(errno) << '\n';
+        return 1;   
+    }
+    
     return 0;
 }
