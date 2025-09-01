14150ed59ed8142ed03d58ef2c2eaedeb7ed3a0f
Rajan Choksi <rachoksi9999@gmail.com>
2025-09-01 23:04:51 +0530

    buffer increase to 1024

--- src/utilities/utilities_one.cpp
+++ src/utilities/utilities_one.cpp
@@ -8,16 +8,16 @@
 
 using namespace UTILITIES_COMMANDS;
 
-int Utilities_commands::print_text(std::string text) 
+int Utilities_commands::print_text(std::string text)
 {
     std::cout << text << '\n';
     return 0;
 }
 
-int Utilities_commands::find_pattern(std::string pattern, const char *path) 
+int Utilities_commands::find_pattern(std::string pattern, const char *path)
 {
     // checking file if it exist and also if it is readable
-    if (access(path, F_OK) == -1 || access(path, R_OK) == -1) 
+    if (access(path, F_OK) == -1 || access(path, R_OK) == -1)
     {
         std::cerr << "-> Error occured, " << strerror(errno) << '\n';
         return 1;
@@ -26,41 +26,42 @@ int Utilities_commands::find_pattern(std::string pattern, const char *path)
     // creating file descriptor for opening file to read it later
     int fd;
     fd = open(path, O_RDONLY);
-    if (fd == -1) 
+    if (fd == -1)
     {
         std::cerr << "-> Error occured, " << strerror(errno) << '\n';
         return 1;
     }
 
     ssize_t buffer_read;
-    char buffer[64];
+    char buffer[1024];
 
     // test code
     int count = 0;
 
-    while ((buffer_read = read(fd, buffer, sizeof(buffer))) > 0) 
+    while ((buffer_read = read(fd, buffer, sizeof(buffer))) > 0)
     {
-        count++; 
+        count++;
+
         std::cout << count << "-> ";
         std::cout << buffer << '\n';
     }
 
-    if (buffer_read == -1) 
+    if (buffer_read == -1)
     {
         std::cerr << "-> Error occured, " << strerror(errno) << '\n';
-        if (close(fd) == -1) 
+        if (close(fd) == -1)
         {
             std::cerr << "-> Error occured, " << strerror(errno) << '\n';
-            return 1;   
+            return 1;
         }
         return 1;
     }
 
-    if (close(fd) == -1) 
+    if (close(fd) == -1)
     {
         std::cerr << "-> Error occured, " << strerror(errno) << '\n';
-        return 1;   
+        return 1;
     }
-    
+
     return 0;
 }
